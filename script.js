4307ab8b5a35c0e5bc3e503da78b597eb5b3dece
ronykader <rony.ksr06@gmail.com>
2022-08-22 17:56:56 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,168 @@
+// Initial State
+const initialState = {
+    id: 0,
+    value: 0,
+    incrementBy: 1,
+    decrementBy: 1
+}
+
+
+
+// Action Identifier
+const ADD_COUNTER = 'addCounter';
+const RESET_COUNTERS = 'resetCounters';
+const INCREMENT = 'increment';
+const DECREMENT = 'decrement';
+
+
+// action creators
+const addCounter = () => {
+    return {
+        type: ADD_COUNTER
+    }
+}
+
+
+const resetCounters = () => {
+    return {
+        type: RESET_COUNTERS
+    }
+}
+
+const increment = (counterId, value) => {
+    return {
+        type: INCREMENT,
+        payload: value
+    }
+}
+
+const decrement = (counterId, value) => {
+    return {
+        type: DECREMENT,
+        payload: value
+    }
+}
+
+// Helper Function
+function nextCounterId(counters){
+    const maxId = counters.reduce(
+        (maxId, counter) => Math.max(counter.id, maxId),
+        -1
+    );
+
+    return maxId + 1;
+}
+
+function incrementHandeler(id, incrementBy){
+    store.dispatch(increment(id,incrementBy));
+}
+function decrementHandeler(id, incrementBy){
+    store.dispatch(decrement(id,incrementBy));
+}
+
+
+
+
+// Create Reducer method
+ function counterReducer(state = initialState, action) {
+    if(action.type === ADD_COUNTER) {
+        return [
+            ...state,
+            {
+                id: nextCounterId(state),
+                value: 0,
+                incrementBy: Math.floor(Math.random()*10) + 1, //Returns random integer from 1 to 9,
+                decrementBy: Math.floor(Math.random()*10) + 1, //Returns random integer from 1 to 9,
+                
+            }
+        ];
+    }  
+    
+    if( action.type === RESET_COUNTERS ){
+        
+        return state.map( (counter) => ({
+            ...counter,
+            value: 0
+        }));
+    } 
+
+
+    if( action.type === INCREMENT){
+        const {counterId, value} = action.payload;
+        return state.map((counter) => {
+            if(counter.id  == counterId){
+                return {
+                    ...counter,
+                    value: counter.value + value,
+                };
+            }else{
+                return {
+                    ...counter
+                }
+            }
+        });
+    }
+
+    if( action.type === DECREMENT){
+        const {counterId, value} = action.payload;
+        return state.map((counter) => {
+            if(counter.id  == counterId){
+                return {
+                    ...counter,
+                    value: counter.value - value,
+                };
+            }else{
+                return {
+                    ...counter
+                }
+            }
+        });
+    }
+
+
+ }
+
+
+// Call to Subscribe
+
+// Create Store
+
+const store = Redux.createStore(counterReducer);
+
+
+// Select Dom Elements
+
+const countersContainer = document.getElementById('counters-container');
+const addCounterButton = document.getElementById('add-counter');
+const resetCounterButton = document.getElementById('reset-counter');
+
+
+
+function render() {
+    const state = store.getState();
+
+    counterEl.innerText = state.count.toString();
+
+}
+
+store.subscribe(render);
+
+
+
+// Handle Event Listner
+incrementEl.addEventListener('click', ()=>{
+
+    store.dispatch({
+        type: 'increment'
+    });
+});
+
+
+decrementEl.addEventListener('click', ()=>{
+
+    store.dispatch({
+        type: 'decrement'
+    });
+});
+
+
